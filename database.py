88620fbac1d70eca2ef0a13202c7c55fd1fdce71
a-garriga <134711795+a-garriga@users.noreply.github.com>
2025-02-17 13:11:09 -0500

    Update database.py

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root", 
-        password="password",
+        password="",
         database="manuDB"
     )
 
