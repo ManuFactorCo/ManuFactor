9b27de3618783feb2057f2e7bc29be7669d1d8bf
LUDGINIE  DORVAL <80278412+Ludginie@users.noreply.github.com>
2025-02-19 14:00:38 -0500

    Update database.py

--- database.py
+++ database.py
@@ -12,31 +12,6 @@ def initialize_database():
     cursor = connection.cursor()
 
 
-    # create product  table (if not exists)
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS product_cvp (
-        prod_id INT PRIMARY KEY AUTO_INCREMENT,
-        comp_id INT,
-        fixed_cost DECIMAL(15,2),
-        variable_cost_per_unit DECIMAL(10,2),
-        selling_price_per_unit DECIMAL(15,2),
-        target_income DECIMAL(15,2)
-    );
-    """)
-
-    # Create User table 
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS user (
-        user_id INT PRIMARY KEY AUTO_INCREMENT,
-        comp_id INT,
-        fname VARCHAR(255),
-        lname VARCHAR(255),
-        email VARCHAR(255),
-        security_level INT,
-        password VARCHAR(255),
-        Username VARCHAR(255) UNIQUE
-    );
-    """)
 
     # adding the default users 
     cursor.execute("SELECT * FROM user WHERE Username = %s", (cipher.encrypt(b"admin").decode(),))
@@ -62,27 +37,6 @@ def add_user_to_database(cursor, username, password, security_level):
     INSERT INTO user (username, lname, email, security_level, password)
     VALUES (%s, %s, %s, %s, %s)
     """, (username, username, f"{username}@example.com", security_level, encrypted_password))
-
-
-'''def get_username_from_database(encrypted_username):
-    connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="manuDB"
-    )
-    cursor = connection.cursor(dictionary=True)
-
-    query = "SELECT * FROM user WHERE username = %s"
-    cursor.execute(query, (encrypted_username,))
-    user = cursor.fetchone()
-
-    #if user:
-       # print("Stored Encrypted Username in DB:", user['username'])  # Debugging line
-
-    cursor.close()
-    connection.close()
-    return user'''
 
 def get_username_from_database(encrypted_username):
     connection = mysql.connector.connect(
