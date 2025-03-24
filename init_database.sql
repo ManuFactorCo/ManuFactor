54ebc670e509a3fecd99713e9a5dcbf06051bdd1
a-garriga <134711795+a-garriga@users.noreply.github.com>
2025-03-24 12:25:56 -0400

    Update init_database.sql

--- init_database.sql
+++ init_database.sql
@@ -20,3 +20,15 @@ CREATE TABLE user (
     lname VARCHAR(15),
     email VARCHAR(20)
 )
+
+CREATE TABLE product_bp (
+    prod_id INT PRIMARY KEY AUTO_INCREMENT,
+    comp_id INT NOT NULL CHECK (comp_id > 0),
+    curr_Sales INT NOT NULL CHECK (curr_Sales > 0),
+    next_Sales INT NOT NULL CHECK (next_Sales > 0),
+    twicenext_Sales INT NOT NULL CHECK (twicenext_Sales > 0),
+    EI_Rate DECIMAL(10,2) NOT NULL CHECK (EI_Rate > 0),
+    DM_per_Unit INT NOT NULL CHECK (DM_per_Unit > 0),
+    EI_DM_Rate DECIMAL(10,2) NOT NULL CHECK (EI_DM_Rate > 0),
+    DM_Price DECIMAL(10,2) NOT NULL CHECK (DM_Price > 0)
+);
