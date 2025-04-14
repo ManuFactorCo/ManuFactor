6902ed032ae5f8fd071f1dd4c5c2236af6eea65f
a-garriga <134711795+a-garriga@users.noreply.github.com>
2025-04-14 12:19:36 -0400

    Update product_bp.py
    refactored so that there is a callable function, calc_bp_summary to be used in app.py

--- product_bp.py
+++ product_bp.py
@@ -88,7 +88,7 @@ def calc_purchases(curr_production, next_production, EI_DM_Rate, DM_per_Unit, DM
 
 def calc_bp_summary(company_id)
         data = get_product_bp(company_id)
-
+    
         if not data:
             return None
     
